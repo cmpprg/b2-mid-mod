4e419da1841ea1e0e56f306145c4ae23c4b9a233
Ryan Camp <campryan@comcast.net>
2020-04-03 11:37:28 -0600

    Write modle test #add_ride for Mechanic

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Mechanic, type: :model do
     it {should have_many :ride_mechanics}
     it {should have_many(:rides).through(:ride_mechanics)}
   end
+
+  describe "instance methods" do
+    it "can create a ride_mechanics record" do
+      amusement_park = AmusementPark.create(name:"Ryan's Adventures", admission_price: 60)
+
+      mechanic1 = Mechanic.create(name:"Ralph", years_of_experience: 21)
+
+      ride1 = amusement_park.rides.create(name:"Lightning Racer", thrill_rating: 9)
+
+      expect(mechanic1.rides.empty?).to eql(true)
+
+      mechanic1.add_ride(ride1.id)
+
+      expect(mechanic1.rides.last).to eql(ride1)
+    end
+  end
 end
