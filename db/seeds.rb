78d3adfa6ca829c07fc462a5d1a84361d5d6da4d
Ryan Camp <campryan@comcast.net>
2020-04-03 09:42:53 -0600

    Set up seeds for use in tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+amusement_park = AmusementPark.create(name:"Ryan's Adventures", admission_price: 60)
+
+mechanic1 = Mechanic.create(name:"Ralph", years_of_experience: 21)
+mechanic2 = Mechanic.create(name:"Rhonda", years_of_experience: 12)
+mechanic3 = Mechanic.create(name:"Sally", years_of_experience: 3)
+mechanic3 = Mechanic.create(name:"Roger", years_of_experience: 7)
+
+ride1 = amusement_park.rides.create(name:"Lightning Racer", thrill_rating: 9)
+ride2 = amusement_park.rides.create(name:"Storm Runner", thrill_rating: 7)
+ride3 = amusement_park.rides.create(name:"The Greate Bear", thrill_rating: 5)
+
+RideMechanic.create(ride:ride1, mechanic:mechanic1)
+RideMechanic.create(ride:ride1, mechanic:mechanic2)
+RideMechanic.create(ride:ride2, mechanic:mechanic2)
+RideMechanic.create(ride:ride2, mechanic:mechanic3)
+RideMechanic.create(ride:ride3, mechanic:mechanic3)
+RideMechanic.create(ride:ride3, mechanic:mechanic4)
---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "When I visit a mechanics index page.", type: :feature do
+  it "I see a header saying 'All Mechanics'." do
+    
+  end
+end
