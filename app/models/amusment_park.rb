1bba93eb2761258e550ae851bf7601b109d310d7
Ryan Camp <campryan@comcast.net>
2020-04-03 09:23:03 -0600

    Create and run amusement park and mechanic tables in db

--- app/models/amusment_park.rb
+++
@@ -1,3 +0,0 @@
-class AmusmentPark < ApplicationRecord
-  
-end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe AmusmentPark, type: :model do
+RSpec.describe AmusementPark, type: :model do
   describe "relationships" do
     it {should have_many :rides}
   end
