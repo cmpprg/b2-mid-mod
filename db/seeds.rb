c526bf6b3000a2ab5614d44484b00b5c7f3d4225
Ryan Camp <campryan@comcast.net>
2020-04-03 10:09:26 -0600

    Write features test for amusement park show page, check name and price of admission

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ mechanic3 = Mechanic.create(name:"Roger", years_of_experience: 7)
 
 ride1 = amusement_park.rides.create(name:"Lightning Racer", thrill_rating: 9)
 ride2 = amusement_park.rides.create(name:"Storm Runner", thrill_rating: 7)
-ride3 = amusement_park.rides.create(name:"The Greate Bear", thrill_rating: 5)
+ride3 = amusement_park.rides.create(name:"Great Bear, The", thrill_rating: 5)
 
 RideMechanic.create(ride:ride1, mechanic:mechanic1)
 RideMechanic.create(ride:ride1, mechanic:mechanic2)
---
+++ spec/features/amusement_park/show_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "When I visit an amusement park's show page", type: :feature do
+  before(:each) do
+    @amusement_park = AmusementPark.create(name:"Ryan's Adventures", admission_price: 60)
+    @ride1 = @amusement_park.rides.create(name:"Lightning Racer", thrill_rating: 9)
+    @ride2 = @amusement_park.rides.create(name:"Storm Runner", thrill_rating: 7)
+    @ride3 = @amusement_park.rides.create(name:"Great Bear, The", thrill_rating: 5)
+  end
+
+  it "I see name and admission price for amusement park" do
+    visit "/amusement_parks/#{@amusement_park.id}"
+
+    within(".park-info") do
+      expect(page).to have_content(@amusement_park.name)
+      expect(page).to have_content("Admissions: $#{@amusement_park.admission_price}.00")
+    end
+  end
+
+  xit "I see names of all the rides for amusement park listed alphabetically" do
+
+  end
+
+  xit "I see average thrill rating of amusement park's rides" do
+
+  end
+end
