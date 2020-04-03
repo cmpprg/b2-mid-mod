513863f543085667a8b8efc3920df7eef747fb34
Ryan Camp <campryan@comcast.net>
2020-04-03 11:39:17 -0600

    Create #add_ride for Mechanic model

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+
+  def add_ride(ride_id)
+    RideMechanic.create(ride_id: ride_id, mechanic: self)
+  end
 end
